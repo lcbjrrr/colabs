0760ca0ff6fa73f89c6a1ed31cda5d89235e09b2
Luiz Barboza <62079708+lcbjrrr@users.noreply.github.com>
2024-03-05 15:59:41 -0600

    Add files via upload

---
+++ scenarios.sql
@@ -0,0 +1,4 @@
+INSERT INTO `scenarios` VALUES
+(1,'Equal Participation',1,'2017-03-02 08:22:27','2017-03-02 08:22:27',NULL),
+(2,'Group Domination',3,'2017-03-02 08:22:27','2017-03-08 05:50:20',NULL),
+(3,'Social Loafing',4,'2017-03-02 08:22:27','2017-03-08 05:50:57',NULL);
