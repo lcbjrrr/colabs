c74ae5e38526551f602bddd21cc8b110f7c66f44
Luiz Barboza <62079708+lcbjrrr@users.noreply.github.com>
2024-03-05 10:52:25 -0600

    Add files via upload

---
+++ consent_forms.sql
@@ -0,0 +1,3 @@
+INSERT INTO `consent_forms` VALUES
+(1,'CoLab: Collaborative Learning Platform',1,'2017-08-28 01:12:46','2019-05-24 09:20:33','<h1> </li>','2017-08-22','2019-08-21',1,'<h1>',0),
+(2,'CoLab: Collaborative Learning Platform_MSMC',1,'2019-09-16 23:53:29','2021-08-24 19:34:11','<h>','2019-09-16','2023-09-22',1,'xxx',0);
