9e2547f5867026b71deadd81fff47ef543c28452
Luiz Barboza <62079708+lcbjrrr@users.noreply.github.com>
2024-03-05 16:10:54 -0600

    Add files via upload

---
+++ styles.sql
@@ -0,0 +1,4 @@
+INSERT INTO `styles` VALUES
+(1,'Default','new','2017-03-02 08:22:26','2017-03-02 08:22:26',NULL),
+(2,'Sliders (simple)','slider_basic','2017-03-02 08:22:26','2017-03-02 08:22:26',NULL),
+(3,'buttons (simple)','button_basic','2017-03-02 08:22:26','2017-03-02 08:22:26',NULL);
