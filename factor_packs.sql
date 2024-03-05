7de8f844225fc889d2e6362733e97845685330c8
Luiz Barboza <62079708+lcbjrrr@users.noreply.github.com>
2024-03-05 14:07:51 -0600

    Add files via upload

---
+++ factor_packs.sql
@@ -0,0 +1,4 @@
+INSERT INTO `factor_packs` VALUES
+(1,'Simple','Borrowed from Goldfinch','2017-03-02 08:22:25','2017-08-17 01:22:58','단순한','Goldfinch에서 빌린'),
+(2,'Original','My earliest formulation','2017-03-02 08:22:25','2017-08-17 01:22:58','실물','나의 가장 초기의 배합'),
+(3,'Distilled I','Distillation of factors from 23 sources','2017-03-02 08:22:26','2017-08-17 01:22:58','증류 한','23 개 출처의 요인 증류');
