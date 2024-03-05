c694f33069cdca9b313942f33d81765a24475fd1
Luiz Barboza <62079708+lcbjrrr@users.noreply.github.com>
2024-03-05 10:14:18 -0600

    Add files via upload

---
+++ candidate_feedbacks.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO `candidate_feedbacks` VALUES
+(1,'Acceptable','2017-04-20 14:04:58','2019-06-26 18:29:00',NULL,'Youve accurately identified and defined an important term related  to the stated topic.\n',NULL,100,1),
+(2,'Definition: Incorrect','2017-04-20 14:04:58','2019-06-26 18:29:00',NULL,'Youve identified an important term related to the stated topic, but the definition is wrong.\n',NULL,50,2),
+(3,'Definition: Almost correct','2017-04-20 14:04:58','2019-09-23 21:18:55',NULL,'Youve identified an important term related to the stated topic and provided a definition that is close to complete, but is lacking in some crucial way(s).',NULL,75,2),
+(4,'Definition: Not Understood','2017-04-20 14:04:58','2019-06-26 18:29:00',NULL,'Youve identified an important term related to the stated topic, but I was unable to understand the definition as youve provided it.\n',NULL,50,2),
+(5,'Definition: Borrowed Word-for-Word','2017-04-20 14:04:58','2019-09-23 21:18:55',NULL,'Youve identified an important term related to the stated topic, but I recognize the definition provided as having been substantially copied directly from another source.\n',NULL,25,2),
+(6,'Term: Too Generic','2017-04-20 14:04:58','2017-10-13 06:42:39',NULL,'The term is not specific to the topic or the course.\n',NULL,10,3),
+(7,'Term: Too Obvious','2017-04-20 14:04:59','2017-10-13 06:42:39',NULL,'The term does not represent new learning or consideration relevant to the topic.\n',NULL,10,3),
+(8,'Term: Invalid/Incorrect','2017-05-21 02:58:08','2017-10-17 06:11:07',NULL,'The term is either not a real term or is being misused.\n',NULL,20,3),
+(9,'Term: Not Relevant','2017-05-21 02:58:23','2017-10-13 06:42:39',NULL,'The term may be interesting, but not relevant to the topic.\n',NULL,30,3),
+(10,'Term: Doesnt match','2017-05-30 06:12:23','2017-10-13 06:42:39',NULL,'The term does not match the definition.\n',NULL,40,3),
+(12,'Term: Proper Name or Product Name','2017-09-26 05:50:44','2019-09-23 21:18:55',NULL,'Proper names and products should not be used unless they are dominant to the point of being synonymous with a class of activity or a household name.',NULL,30,3),
+(13,'Definition: Wrong Context','2017-10-13 06:42:39','2019-06-26 18:29:00',NULL,'Youve identified an important term related to the stated topic, but the definition youve provided, while correct for other uses, is not correct for this topic.\n',NULL,50,2),
+(14,'Term: Too specific','2017-10-28 16:19:19','2017-10-28 16:19:19',NULL,'Terms ought to be at a conceptual level. Terms that are specific to a specific implementation should generally be avoided.\n',NULL,30,3),
+(15,'Definition: Recursive','2019-03-14 02:42:37','2019-06-26 18:29:00',NULL,'The term was used to define itself.\n',NULL,50,2);
